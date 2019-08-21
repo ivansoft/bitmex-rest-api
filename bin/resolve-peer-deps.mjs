ddee881febadb9a5e4550d68fa14191d13fcd8a6
ivan.zemskiy <ivan.zemskiy@yahoo.com>
2019-08-21 03:22:59 +0300

    remove "peerDependencies" from package.json

--- bin/resolve-peer-deps.mjs
+++ bin/resolve-peer-deps.mjs
@@ -11,12 +11,7 @@ Output:
 
 node-pre-gyp WARN Using request for node-pre-gyp https download
 [fsevents] Success: "./node_modules/jest-haste-map/node_modules/fsevents/lib/binding/Release/node-v72-darwin-x64/fse.node" is installed via remote
-npm WARN bitmex-rest-api@0.0.1 requires a peer of superagent@^5.1.0 but none is installed. You must install peer dependencies yourself.
-npm WARN bitmex-rest-api@0.0.1 requires a peer of mime@^2.0.3 but none is installed. You must install peer dependencies yourself.
-npm WARN bitmex-rest-api@0.0.1 requires a peer of extend@^3.0.2 but none is installed. You must install peer dependencies yourself.
-npm WARN bitmex-rest-api@0.0.1 requires a peer of fsevents@latest but none is installed. You must install peer dependencies yourself.
-
-added 620 packages from 410 contributors and audited 875555 packages in 10.882s
+added 620 packages from 410 contributors and audited 875555 packages in 11.011s
 found 2 vulnerabilities (1 low, 1 moderate)
   run `npm audit fix` to fix them, or `npm audit` for details
 
